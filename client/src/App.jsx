99cbc9de35c1b5b47a9b406f3d279c54d5d9858d
Adhi Setyatuhu <adhi.tuhu@gmail.com>
2024-12-16 21:20:55 +0700

    feat: consume api and show the result as PokeCards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,44 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
+import PokeCard from './components/PokeCard.jsx'
+
+// buat github organization [done]
+// buat repository [done]
+// init project vite [done]
+// setup tailwindcss 
+// consume pokeapi [done]
+// display ke layout card [done]
 
 function App() {
-  const [count, setCount] = useState(0)
+  const baseUrl = 'https://pokeapi.co/api/v2/';
+  const [pokeList, setPokeList] = useState([]);
+
+  const fetchPokeList = async () => {
+    const offset = 0;
+    try {
+      const { data } = await axios.get(baseUrl + 'pokemon?limit=24&offset=' + offset);
+      setPokeList(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchPokeList();
+  }, [])
+
+  useEffect(() => { })
 
   return (
     <>
-      <h1>Hello</h1>
+      {
+        pokeList.results?.map((pokemon, index) => {
+          return <PokeCard key={index} data={pokemon} />
+        })
+      }
     </>
   )
 }
