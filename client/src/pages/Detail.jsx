9c436b84b327f9bbffc8194ea54171729d89192b
Adhi Setyatuhu <adhi.tuhu@gmail.com>
2024-12-18 21:03:17 +0700

    feat: add radar chart

--- client/src/pages/Detail.jsx
+++ client/src/pages/Detail.jsx
@@ -1,8 +1,49 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, Component } from "react";
 import { useParams } from "react-router";
 import ReactHowler from "react-howler";
 import { LoadingIcon } from "../components/PokeCard";
+import Chart from 'react-apexcharts';
+
+class StatsChart extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            options: {
+                chart: {
+                    id: "basic-bar"
+                },
+                xaxis: {
+                    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+                }
+            },
+            series: [
+                {
+                    name: "series-1",
+                    data: [30, 40, 45, 50, 49, 60, 70, 91]
+                }
+            ]
+        };
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <div className="row">
+                    <div className="mixed-chart">
+                        <Chart
+                            options={this.state.options}
+                            series={this.state.series}
+                            type="radar"
+                            width="500"
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
 
 function Hero(props) {
     const [sound, setSound] = useState(null);
@@ -58,6 +99,8 @@ function Hero(props) {
                             }
                         </tbody>
                     </table>
+
+                    <StatsChart />
 
                 </div>
                 {/* end base stats */}
