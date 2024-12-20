a821bdec997d4b1adef97b2de241c90cea8048b3
Adhi Setyatuhu <adhi.tuhu@gmail.com>
2024-12-20 17:56:01 +0700

    feat: hide prev button when at  page 1

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Pagination({ pokeList, setCurrentUrl }) {
   return (
     <div className='mt-5'>
-      <span className='px-4 py-2 rounded-md bg-blue-500 text-white hover:cursor-pointer hover:bg-blue-400 font-semibold' onClick={() => { pokeList.previous ? setCurrentUrl(pokeList.previous) : null }}>Prev</span>
-      <span className='ml-2 px-4 py-2 rounded-md bg-blue-500 text-white hover:cursor-pointer hover:bg-blue-400 font-semibold' onClick={() => { pokeList.next ? setCurrentUrl(pokeList.next) : null }}>Next</span>
+      <span className={pokeList.previous ? 'px-4 py-2 mr-2 rounded-md bg-blue-500 text-white hover:cursor-pointer hover:bg-blue-400 font-semibold' : 'hidden'} onClick={() => { pokeList.previous ? setCurrentUrl(pokeList.previous) : null }}>Prev</span>
+      <span className='px-4 py-2 rounded-md bg-blue-500 text-white hover:cursor-pointer hover:bg-blue-400 font-semibold' onClick={() => { pokeList.next ? setCurrentUrl(pokeList.next) : null }}>Next</span>
     </div>
   )
 }
