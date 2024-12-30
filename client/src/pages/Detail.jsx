a307e41ff13a78b65066a73f7fb144825c5391f1
Adhi Setyatuhu <adhi.tuhu@gmail.com>
2024-12-30 09:13:05 +0700

    chore: remove unused import

--- client/src/pages/Detail.jsx
+++ client/src/pages/Detail.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState, Component } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import ReactHowler from "react-howler";
 import { LoadingIcon } from "../components/PokeCard";
